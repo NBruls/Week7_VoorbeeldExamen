b269afb156715460d617296f05a170e2fa27a4bf
NikkiBrulsPXL <11900461@student.pxl.be>
2020-03-24 16:42:05 +0100

    solved oef3

--- oef3/js/rekenen.js
+++ oef3/js/rekenen.js
@@ -11,10 +11,36 @@ function handleLoad () {
 }
 
 
-function handleClick () { 
+function handleClick () {
+	let ingave = document.getElementById("input_aantal").value;
+	let output = document.getElementById("vermenigvuldigingen");
+	if (! /^\d+$/.test(ingave)){
+		output.appendChild(document.createTextNode("misse ingave voor aantal"));
+	} else {
+		let numberOfExercises = parseInt(ingave);
+		output.appendChild(document.createElement("hr"));
+		for (let i = 0; i < numberOfExercises; i++){
+			let div = document.createElement("div");
+			let getal1 = parseInt(`${10*Math.random()}`);
+			let getal2 = parseInt(`${10*Math.random()}`);
+			let input = document.createElement("input");
+			input.type = "text";
+			input.onkeyup = handleKeyupInput;
+			div.appendChild(document.createTextNode(`${getal1} * ${getal2} = `));
+			div.appendChild(input);
+			output.appendChild(div);
+			output.appendChild(document.createElement("hr"));
+		}
+	}
 }
 
 function handleKeyupInput(event){
-
+	let target = event.target;
+	let value = target.value;
+	if (! /^\d+$/.test(value)) {
+		target.style.color = "red";
+	} else {
+		target.style.color = "black";
+	}
 }
 

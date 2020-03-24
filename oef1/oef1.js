4ba1efa762ed6bed0b254da5c9dbe656181e8c56
NikkiBrulsPXL <11900461@student.pxl.be>
2020-03-24 14:21:27 +0100

    solved oef1

--- oef1/oef1.js
+++ oef1/oef1.js
@@ -1,4 +1,119 @@
-// naam: 
+// naam: Nikki Bruls
+
+class InputError extends Error{}
+
+function checkValue(value) {
+    if (Number.isInteger(value) && value > 0){
+        return true
+    } else {
+        throw new InputError("Foute ingave! Geef een geheel getal groter dan 0 in!");
+    }
+}
+
+class Persoon {
+    constructor(id, naam){
+        this._id = id;
+        this._naam = naam;
+    }
+
+    get naam() {
+        return this._naam;
+    }
+
+    set naam(value) {
+        if (typeof value == "string") {
+            this._naam = value;
+        } else {
+            throw new InputError("Foute ingave! Naam moet een string zijn!");
+        }
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        if (checkValue(value)) {
+            this._id = value;
+        }
+    }
+
+    toString() {
+        return `[${this.id}] ${this.naam}`;
+    }
+}
+
+class Loonwerker extends Persoon{
+    constructor(id, naam, loonPerUur, aantalUrenGewerkt) {
+        super(id, naam);
+        this.loonPerUur = loonPerUur;
+        this.aantalUrenGewerkt = aantalUrenGewerkt;
+    }
+
+    get aantalUrenGewerkt() {
+        return this._aantalUrenGewerkt;
+    }
+
+    set aantalUrenGewerkt(value) {
+        if (checkValue(value)) {
+            this._aantalUrenGewerkt = value;
+        }
+    }
+
+    get loonPerUur() {
+        return this._loonPerUur;
+    }
+
+    set loonPerUur(value) {
+        if (checkValue(value)) {
+            this._loonPerUur = value;
+        }
+    }
+
+    berekenLoon() {
+        return this.aantalUrenGewerkt * this.loonPerUur;
+    }
+
+    toString() {
+        return `${super.toString()} = ${this.berekenLoon()}`;
+    }
+}
+
+class Manager extends Persoon {
+    constructor(id, naam) {
+        super(id, naam);
+        this.loonWerkers = [];
+    }
+
+
+    get loonWerkers() {
+        return this._loonWerkers;
+    }
+
+    set loonWerkers(value) {
+        this._loonWerkers = value;
+    }
+
+    voegLoonWerkerToe(loonWerker) {
+        if (loonWerker instanceof Loonwerker) {
+            this._loonWerkers.push(loonWerker);
+        } else {
+            throw new InputError("Foute ingave! Waarde moet een instantie van de klasse LoonWerker zijn!");
+        }
+    }
+
+    berekenLoon() {
+        let som = 0;
+        for (let i = 0; i < this.loonWerkers.length; i++) {
+            som += this.loonWerkers[i].berekenLoon();
+        }
+        return Math.round(som * 0.2);
+    }
+
+    toString() {
+        return `${super.toString()} = ${this.berekenLoon()}`;
+    }
+}
 
 let persoon = new Persoon(1,"mieke");
 let manager=new Manager(2,"jan");
